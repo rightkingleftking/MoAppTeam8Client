3f5e68dda047d14379bbf072f51560f28e4121c8
oms801 <46886592+oms801@users.noreply.github.com>
2020-11-26 03:34:14 +0900

    setting, marketselect add

---
+++ app/src/main/java/com/example/teamproject/Setting.java
@@ -0,0 +1,19 @@
+package com.example.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+    public void onBackbuttonClicked(View view) {
+        finish();
+    }
+}
